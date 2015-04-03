6fb8d0c47b35c5088295490621499828c325d3e5
Anthropohedron <github.20.gseidman@spamgourmet.com>
2015-04-03 16:54:52 -0400

    validation of request/response, tests for it

--- test/test-tagmaps.js
+++ test/test-tagmaps.js
@@ -23,12 +23,62 @@ exports["test create invalid matcher"] = function(assert) {
 };
 
 exports["test complete match request tag"] = function(assert) {
-  var channel = { foo: "bar" };
-  var matcher = tagMaps.tagMatchForPattern("TAG:^"+fakeUserAgent);
+  var channel = {};
+  var matcher = tagMaps.tagMatchForPattern("TAG:^"+fakeUserAgent+"$");
   assert.ok(matcher, "Created the tag matcher");
   tagMaps.addRequestTag(channel, fakeUserAgent);
   assert.pass("Added user agent tag");
   assert.strictEqual(matcher.testRequest(channel), true, "Matched tag");
 };
 
+exports["test complete match response tag"] = function(assert) {
+  var channel = {};
+  var matcher = tagMaps.tagMatchForPattern("TAG:^"+fakeUserAgent+"$");
+  assert.ok(matcher, "Created the tag matcher");
+  tagMaps.addResponseTag(channel, fakeUserAgent);
+  assert.pass("Added user agent tag");
+  assert.strictEqual(matcher.testResponse(channel), true, "Matched tag");
+};
+
+exports["test complete match no-request-tag request"] = function(assert) {
+  var channel = {};
+  var matcher = tagMaps.tagMatchForPattern("NO-REQUEST-TAG:^"+fakeUserAgent+"$");
+  assert.ok(matcher, "Created the tag matcher");
+  tagMaps.addRequestTag(channel, fakeUserAgent);
+  assert.pass("Added user agent tag");
+  assert.strictEqual(matcher.testRequest(channel), true, "Matched tag");
+};
+
+exports["test complete match no-response-tag response"] = function(assert) {
+  var channel = {};
+  var matcher = tagMaps.tagMatchForPattern("NO-RESPONSE-TAG:^"+fakeUserAgent+"$");
+  assert.ok(matcher, "Created the tag matcher");
+  tagMaps.addResponseTag(channel, fakeUserAgent);
+  assert.pass("Added user agent tag");
+  assert.strictEqual(matcher.testResponse(channel), true, "Matched tag");
+};
+
+exports["test complete match no-response-tag request"] = function(assert) {
+  var channel = {};
+  var matcher = tagMaps.tagMatchForPattern("NO-RESPONSE-TAG:^"+fakeUserAgent+"$");
+  assert.ok(matcher, "Created the tag matcher");
+  tagMaps.addRequestTag(channel, fakeUserAgent);
+  assert.pass("Added user agent tag");
+  assert.throws(function() {
+    matcher.testRequest(channel);
+  }, /NO-RESPONSE-TAG .* request/, "Matching a response against a request");
+};
+
+exports["test complete match no-request-tag response"] = function(assert) {
+  var channel = {};
+  var matcher = tagMaps.tagMatchForPattern("NO-REQUEST-TAG:^"+fakeUserAgent+"$");
+  assert.ok(matcher, "Created the tag matcher");
+  tagMaps.addResponseTag(channel, fakeUserAgent);
+  assert.pass("Added user agent tag");
+  assert.throws(function() {
+    matcher.testResponse(channel);
+  }, /NO-REQUEST-TAG .* response/, "Matching a request against a response");
+};
+
 require("sdk/test").run(exports);
+
