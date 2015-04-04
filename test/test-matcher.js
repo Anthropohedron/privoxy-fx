6b4589736fbef47cc946475dd26270b538a95a17
Anthropohedron <github.20.gseidman@spamgourmet.com>
2015-04-04 13:06:50 -0400

    validation and a test for it

--- test/test-matcher.js
+++ test/test-matcher.js
@@ -1,6 +1,6 @@
 var Matcher = require("./matcher").Matcher;
 
-exports["test create path matcher"] = function(assert) {
+exports["test create matcher"] = function(assert) {
   var patterns = [
     "TAG:^Content-Type: image/",
     "NO-RESPONSE-TAG:^User-Agent: .*Internet",
@@ -14,5 +14,14 @@ exports["test create path matcher"] = function(assert) {
   assert.equal(matcher.patterns && matcher.patterns.length,
       patterns.length, "Correct number of patterns");
 };
+
+exports["test create match no patterns"] = function(assert) {
+  assert.throws(function() {
+    new Matcher();
+  }, /no patterns/i, "No arguments to Matcher constructor");
+  assert.throws(function() {
+    new Matcher([]);
+  }, /no patterns/i, "Empty array passed to Matcher constructor");
+}
 
 require("sdk/test").run(exports);
