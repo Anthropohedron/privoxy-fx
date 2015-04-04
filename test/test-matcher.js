d1e92a1eff8d84bbd0fc08b800c8ec842d7e0997
Anthropohedron <github.20.gseidman@spamgourmet.com>
2015-04-04 12:57:17 -0400

    more matcher tests

--- test/test-matcher.js
+++ test/test-matcher.js
@@ -1,14 +1,18 @@
 var Matcher = require("./matcher").Matcher;
 
 exports["test create path matcher"] = function(assert) {
-  var matcher = new Matcher([
+  var patterns = [
     "TAG:^Content-Type: image/",
     "NO-RESPONSE-TAG:^User-Agent: .*Internet",
     "NO-REQUEST-TAG:^User-Agent: .*Explorer",
     ".example.com/index.html",
     ".example.com"
-  ]);
+  ];
+  var matcher = new Matcher(patterns);
   assert.ok(matcher, "Created a matcher");
+  assert.ok(matcher.patterns, "Has patterns");
+  assert.equal(matcher.patterns && matcher.patterns.length,
+      patterns.length, "Correct number of patterns");
 };
 
 require("sdk/test").run(exports);
