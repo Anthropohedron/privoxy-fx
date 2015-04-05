dab7b175bbf7c386049c2fad57723a5727d90807
Anthropohedron <github.20.gseidman@spamgourmet.com>
2015-04-04 22:14:09 -0400

    new test, fix for exposed bug

--- test/test-matcher.js
+++ test/test-matcher.js
@@ -53,4 +53,33 @@ exports["test exact host match"] = function(assert) {
       "Exact host match response");
 }
 
+exports["test partial host match"] = function(assert) {
+  var channel = createMockChannel("www.example.com", "/");
+  var matcher = new Matcher([ "www." ]);
+  assert.strictEqual(matcher.testRequest(channel), true,
+      "Prefix host match request");
+  assert.strictEqual(matcher.testResponse(channel), true,
+      "Prefix host match response");
+  var matcher = new Matcher([ ".example.com" ]);
+  assert.strictEqual(matcher.testRequest(channel), true,
+      "Suffix host match request");
+  assert.strictEqual(matcher.testResponse(channel), true,
+      "Suffix host match response");
+  var matcher = new Matcher([ ".example." ]);
+  assert.strictEqual(matcher.testRequest(channel), true,
+      "Middle host match request");
+  assert.strictEqual(matcher.testResponse(channel), true,
+      "Middle host match response");
+  var matcher = new Matcher([ "www.exam*.com" ]);
+  assert.strictEqual(matcher.testRequest(channel), true,
+      "Glob host match request");
+  assert.strictEqual(matcher.testResponse(channel), true,
+      "Glob host match response");
+  var matcher = new Matcher([ "www.exa??le.com" ]);
+  assert.strictEqual(matcher.testRequest(channel), true,
+      "Wildcard host match request");
+  assert.strictEqual(matcher.testResponse(channel), true,
+      "Wildcard host match response");
+}
+
 require("sdk/test").run(exports);
