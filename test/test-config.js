e6a0bc27e7d40e6a63a6891b2a034ef5d36414cb
Anthropohedron <github.20.gseidman@spamgourmet.com>
2015-04-07 21:13:33 -0400

    skeleton of configuration

---
+++ test/test-config.js
@@ -0,0 +1,9 @@
+var Configuration = require("./config").Configuration;
+
+exports["test config instantiate"] = function(assert) {
+  var config = new Configuration();
+  assert.ok(config, "Configuration instantiated");
+}
+
+require("sdk/test").run(exports);
+
