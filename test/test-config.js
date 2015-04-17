45eeedea8ee1d93175e0a76793cd002650d37711
Anthropohedron <github.20.gseidman@spamgourmet.com>
2015-04-17 09:43:09 -0400

    started really building config

--- test/test-config.js
+++ test/test-config.js
@@ -1,8 +1,34 @@
-var Configuration = require("./config").Configuration;
+var addonPrefs = require("sdk/simple-prefs").prefs;
+var config = require("./config");
+var defaultConfig = config.getDefaultConfig();
 
-exports["test config instantiate"] = function(assert) {
-  var config = new Configuration();
-  assert.ok(config, "Configuration instantiated");
+function postTestCleanup() {
+  addonPrefs.configJSON = "null";
+  config.setConfig(null);
+}
+
+exports["test config load default"] = function(assert) {
+  assert.ok(!config.getConfig(), "No config initially loaded");
+  assert.ok(!config.loadConfig(), "Default configuration loaded");
+  assert.deepEqual(defaultConfig, config.getConfig(),
+      "Loaded config matches default config");
+  assert.strictEqual(addonPrefs.configJSON,
+      JSON.stringify(config.getConfig()),
+      "Loaded config matches prefs config");
+  postTestCleanup();
+};
+
+exports["test config save config"] = function(assert) {
+  assert.ok(!config.getConfig(), "No config initially loaded");
+  assert.ok(addonPrefs.configJSON === "null", "No config in prefs");
+  assert.ok(config.saveConfig(), "Saved default configuration");
+  assert.strictEqual(addonPrefs.configJSON, JSON.stringify(defaultConfig),
+      "Config stored in prefs");
+  assert.ok(!config.getConfig(), "Still no config loaded");
+  assert.ok(config.loadConfig(), "Loaded config from prefs");
+  assert.deepEqual(defaultConfig, config.getConfig(),
+      "Loaded config matches default config");
+  postTestCleanup();
 };
 
 require("sdk/test").run(exports);
